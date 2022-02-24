3010eae2424f13e8c1709872a2e4b5fdb07c6752
Xenia Padilla <xeniaens@MacBook-Air-de-Xenia.local>
2022-02-23 21:46:34 -0800

    agregando el cascaron

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const AboutMeApp());
+
+class AboutMeApp extends StatelessWidget {
+  const AboutMeApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget{
+  const HomePage({Key?key}):super(key: key);
+  
+  @override  
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
